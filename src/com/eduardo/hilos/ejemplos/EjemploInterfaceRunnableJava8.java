207288c5dffc1726b5aa50bc87991d059f9431fd
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-01-11 01:51:43 -0600

    Se agrega el uso de join

--- src/com/eduardo/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
+++ src/com/eduardo/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
@@ -3,12 +3,12 @@ package com.eduardo.hilos.ejemplos;
 import com.eduardo.hilos.ejemplos.runnable.ViajeTarea;
 
 public class EjemploInterfaceRunnableJava8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Runnable viaje = () -> {
             for (int i = 0; i < 10; i++) {
                 System.out.println(i + " - " + Thread.currentThread().getName());
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -16,9 +16,20 @@ public class EjemploInterfaceRunnableJava8 {
             System.out.println("Finalmente me voy de viaje a " + Thread.currentThread().getName());
         };
 
-        new Thread(viaje, "DGO").start();
-        new Thread(viaje, "Barcelona").start();
-        new Thread(viaje, "Islandia").start();
-        new Thread(viaje, "Nueva Zelanda").start();
+        Thread v1 = new Thread(viaje, "DGO");
+        Thread v2 = new Thread(viaje, "Barcelona");
+        Thread v3 = new Thread(viaje, "Islandia");
+        Thread v4 = new Thread(viaje, "Nueva Zelanda");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+
+        System.out.println("Continuando con la ejecucion del main");
     }
 }
